feae24b605086be3ae1258d34ff87f2a880d2e7c
ALEEM ABDULMUHEEZ Ayomide <aleemabdulmuheez@gmail.com>
2025-09-25 15:52:09 +0100

    Reusable Card Component

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { CardProps } from '@/interfaces'
+function Card({title,content}:CardProps) {
+  return (
+    <div className='border rounded-lg p-4 shadow hover:shadow-lg transition'>
+      <h2 className='text-xl font-bold mb-2'>{title}</h2>
+      <p className='text-gray-700'>{content}</p>
+    </div>
+  )
+}
+
+export default Card
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,9 +1,12 @@
+import Card from '@/components/common/Card'
 import React from 'react'
 
 function Home() {
   return (
-    <div>
+    <div className='p-6 space-y-4'>
       <h1 className="text-2xl font-semibold">This is the Home Page</h1>
+      <Card title='Card One' content='This is the First card.' />
+      <Card title='Card Two' content='This is the Second card.' />
     </div>
   )
 }
