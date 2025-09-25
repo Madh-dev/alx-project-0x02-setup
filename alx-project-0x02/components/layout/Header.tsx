a0ce5dd011d40b4715d36dc47473b9f68a72875f
ALEEM ABDULMUHEEZ Ayomide <aleemabdulmuheez@gmail.com>
2025-09-25 17:15:45 +0100

    Navigation & Layout

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -3,9 +3,10 @@ import Link from 'next/link'
 
 function Header() {
   return (
-    <header className='bg-gray-800 text-white p-4 flex gap-4'>
-      <Link href={"/home"}>Home</Link>
-      <Link href={"/about"}>About</Link>
+     <header className="bg-gray-800 text-white p-4 flex gap-6">
+      <Link href="/home">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/posts">Posts</Link>
     </header>
   )
 }
---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '@/components/layout/Header'
+
+
+function posts() {
+  return (
+     <>
+      <Header />
+      <main className="p-6">
+        <h1 className="text-3xl font-bold">Posts Page 📰</h1>
+        <p>Posts from API will be displayed here soon.</p>
+      </main>
+    </>
+  )
+}
+
+export default posts
